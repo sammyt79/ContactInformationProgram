990f2ed79d14e7d04fd486d3443564c040fcd538
Samuel Tollefson <sam8879@gmail.com>
2015-03-16 22:30:22 -0400

    java2

---
+++ Contact Information Program/Part1/src/Part1.java
@@ -0,0 +1,184 @@
+
+/**
+ * Contact Information Program Samuel Tollefson PRG/421 February 9, 2015 Roland
+ * Morales
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * Part1 creates a GUI that prompts the user for contact name, age,
+ * email, and cell.
+ */
+public class Part1 extends JFrame {
+
+    // The following variables will reference the
+    // labels.
+    private static JLabel messageLabelFirst; // Text field for first name.
+    private static JLabel messageLabelLast; // Text field for last name.
+    private static JLabel messageLabelAge; // Text field for first name.
+    private static JLabel messageLabelEmail; // Text field for last name.
+    private static JLabel messageLabelCell; // Text field for first name.
+
+    // The following variables will reference the
+    // text fields.
+    private static JTextField firstTextField; // Text field for first name.
+    private static JTextField lastTextField; // Text field for last name.
+    private static JTextField ageTextField; // Text field for first name.
+    private static JTextField emailTextField; // Text field for last name.
+    private static JTextField cellTextField; // Text field for first name.
+
+    // The following variables will reference objects
+    // needed to add the Enter and Exit buttons.
+    private static JButton enterButton; // To calculate total compensation
+    private static JButton exitButton; // To exit the application
+
+    // The following variable will reference an object
+    // needed to open a file.
+    PrintWriter outputFile;
+
+    /**
+     * Constructor
+     */
+    public Part1() throws FileNotFoundException {
+
+        // Display a title.
+        super("Contact Information Program");
+
+        // Creates a text file named ContactData.
+        this.outputFile = new PrintWriter("../ContactData.txt");
+
+        // Specify an action for the close button.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a BorderLayout manager for
+        // the content pane.
+        setLayout(new GridLayout(6, 1));
+
+        // Call the buildButtonPanel method to
+        // create the button panel.
+        buildPanel();
+
+        // Pack the contents of the window and display it.
+        pack();
+        setVisible(true);
+    }
+
+    private void buildPanel() {
+
+        // Create labels for first and last name, age, email, and cell.
+        messageLabelFirst = new JLabel("First Name");
+        messageLabelLast = new JLabel("Last Name");
+        messageLabelAge = new JLabel("Age");
+        messageLabelEmail = new JLabel("Email");
+        messageLabelCell = new JLabel("Cell Phone");
+
+        // Create text fields for first and last name, age, email, and cell.
+        firstTextField = new JTextField(10);
+        lastTextField = new JTextField(10);
+        ageTextField = new JTextField(10);
+        emailTextField = new JTextField(10);
+        cellTextField = new JTextField(10);
+
+        enterButton = new JButton("Enter Data"); // To enter data to text file.
+        exitButton = new JButton("Exit"); // Exits the application.
+
+        // Create panels for first and last name, age, email, and cell.
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        // Position the panels.
+        panel1.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel2.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel3.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel4.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel5.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        // Add the labels to the panels.
+        panel1.add(messageLabelFirst);
+        panel2.add(messageLabelLast);
+        panel3.add(messageLabelAge);
+        panel4.add(messageLabelEmail);
+        panel5.add(messageLabelCell);
+
+        // Add the text fields to the panels.
+        panel1.add(firstTextField);
+        panel2.add(lastTextField);
+        panel3.add(ageTextField);
+        panel4.add(emailTextField);
+        panel5.add(cellTextField);
+
+        // Create and position the button panel.
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        // Add buttons to the button panel.
+        buttonPanel.add(enterButton);
+        buttonPanel.add(exitButton);
+
+        // Register the action listeners.
+        enterButton.addActionListener(new EnterButtonListener());
+        exitButton.addActionListener(new ExitButtonListener());
+
+        // Add the panels.
+        add(panel1);
+        add(panel2);
+        add(panel3);
+        add(panel4);
+        add(panel5);
+        add(buttonPanel);
+    }
+
+    /**
+     * Private inner class that handles the event when the user clicks the Enter
+     * button.
+     */
+    private class EnterButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            // Write data to the ContactData file.
+            outputFile.println(firstTextField.getText()
+                    + " "
+                    + lastTextField.getText());
+            outputFile.println(ageTextField.getText());
+            outputFile.println(emailTextField.getText());
+            outputFile.println(cellTextField.getText());
+
+            // Reset the text fields.
+            firstTextField.setText(null);
+            lastTextField.setText(null);
+            ageTextField.setText(null);
+            emailTextField.setText(null);
+            cellTextField.setText(null);
+        }
+    }
+
+    /**
+     * Private inner class that handles the event when the user clicks the Exit
+     * button.
+     */
+    private class ExitButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            // Close the ContactData file.
+            outputFile.close();
+
+            // Exit the application.
+            System.exit(0);
+        }
+    }
+
+    // Main class runs the program.
+    public static void main(String[] args) throws IOException {
+
+        Part1 part1 = new Part1();
+    }
+}
---
+++ Contact Information Program/Part2/src/Part2.java
@@ -0,0 +1,162 @@
+
+/**
+ * Contact Information Program Samuel Tollefson PRG/421 February 9, 2015 Roland
+ * Morales
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Part2 reads the data from the text file created by user input from the Part1
+ * program and displays it in a table.
+ */
+public class Part2 extends JFrame {
+
+    // The following variables will reference the
+    // buttons.
+    private JButton save;
+    private JButton exit;
+
+    // The following variable will reference the
+    // table model.
+    private DefaultTableModel tableModel;
+
+    // Variable to increment donor count,
+    int donarCount = 0;
+
+    // Scanner reference variable.
+    private Scanner input;
+
+    /**
+     * Constructor
+     */
+    public Part2() {
+
+        // Display a title.
+        super("Contact Info Read");
+
+        // Specify an action for the close button.
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Disable resizing.
+        setResizable(false);
+
+        // Create a panel for the buttons.
+        JPanel buttonPanel = new JPanel();
+
+        // Create the table.
+        JTable saveTable = new JTable();
+
+        // Create the save and exit buttons.
+        save = new JButton("Display Contact Info");
+        exit = new JButton("Exit");
+
+        // Register the action listeners.
+        save.addActionListener(new SaveButtonListener());
+        exit.addActionListener(new ExitButtonListener());
+
+        // Add buttons to panel.
+        buttonPanel.add(save);
+        buttonPanel.add(exit);
+
+        // Returns the contentPane object for this frame.
+        Container tableButtonContainer = getContentPane();
+
+        // Set layouts.
+        tableButtonContainer.setLayout(new BorderLayout());
+        tableButtonContainer.setLayout(new BoxLayout(tableButtonContainer,
+                BoxLayout.Y_AXIS));
+
+        // Constructs a DefaultTableModel with rowCount and columnCount.
+        tableModel = new DefaultTableModel(0, 5);
+
+        // Create column identifiers.
+        String col[] = {"Contact Name", "Age", "Email", "Cell Number"};
+        tableModel.setColumnIdentifiers(col);
+        saveTable.setModel(tableModel);
+
+        // Create and set a scroll pane.
+        JScrollPane tableScroller = new JScrollPane(saveTable,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        tableScroller.setViewportView(saveTable);
+        tableButtonContainer.add(saveTable, BorderLayout.CENTER);
+
+        tableButtonContainer.add(buttonPanel, BorderLayout.SOUTH);
+        tableButtonContainer.add(tableScroller);
+        tableScroller.setViewportView(saveTable);
+
+        // Pack the contents of the window and display it.
+        pack();
+        setVisible(true);
+
+    }
+
+    /**
+     * Private inner class that handles the event when the user clicks the
+     * Display Contact Info button.
+     */
+    private class SaveButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            // Test for exceptions.
+            try {
+                
+                // Create file scanner.
+                input = new Scanner(new File("../ContactData.txt"));
+
+                // Display all available data.
+                while (input.hasNext()) {
+                    tableModel.setRowCount(donarCount + 1);
+                    tableModel.setValueAt(input.nextLine(), donarCount, 0);
+                    tableModel.setValueAt(input.nextLine(), donarCount, 1);
+                    tableModel.setValueAt(input.nextLine(), donarCount, 2);
+                    tableModel.setValueAt(input.nextLine(), donarCount, 3);
+
+                    // Increment donorCount variable
+                    donarCount++;
+                }
+                
+            // Catch missing file exception.
+            } catch (FileNotFoundException ex) {
+                
+                // Display error message.
+                JOptionPane.showMessageDialog(null,
+                        "File not found",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                
+                // Exit the application.
+                System.exit(0);
+            } finally {
+                
+                // Close the file. 
+                input.close();
+            }
+        }
+    }
+
+    /**
+     * Private inner class that handles the event when the user clicks the Exit
+     * button.
+     */
+    private class ExitButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            // Exit the application.
+            System.exit(0);
+        }
+    }
+
+    //main class
+    public static void main(String args[]) {
+        Part2 part2 = new Part2();
+    }
+}
